05d14172137ebeb0fbd29740d60fa94538ed6a25
stanciudrg <stanciudragos1337@gmail.com>
2023-11-15 16:51:56 +0200

    Install and configure CSS loader and minifier

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html',
         }),
+        new MiniCssExtractPlugin(),
     ],
+    optimization: {
+        minimizer: [
+            '...',
+            new CssMinimizerPlugin(),
+        ],
+    },
 };
